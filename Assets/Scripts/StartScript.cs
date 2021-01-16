e8645a98b0724d976776276abc4af8410976382e
Hudson <huddy987@gmail.com>
2021-01-16 16:38:08 -0700

    Change Spawner to StartScript

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    public GameObject obj;
+    /* Slider options */
+    public int numInfected = 1;
+    public int numSusceptible = 10;
+    public int numSusceptibleWithMasks = 0;
+    public float infectionRadius = 5.0f;
+    public float infectionChance = 0.5f;
+
+    private bool spawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void spawnSusceptible()
+    {
+        for (int i = 0; i < numSusceptible; i++)
+        {
+            // TODO: Replace obj with susceptible object
+            Instantiate(obj, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        }
+    }
+
+    private void spawnInfected()
+    {
+        for (int i = 0; i < numSusceptible; i++)
+        {
+            // TODO: Replace obj with infected object
+            Instantiate(obj, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
+        {
+            spawned = true;
+            spawnSusceptible();
+            spawnInfected();
+        }
+    }
+}

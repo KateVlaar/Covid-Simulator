de2d61e363b8c0b92f318d08170c2442e737bb6f
Hudson <huddy987@gmail.com>
2021-01-16 13:58:16 -0700

    Add base scripts

---
+++ Assets/Scripts/Infected.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infected : Person
+{
+    private double infectionRadius = 5.0;
+    /* Chance out of 0.5 that a susceptible person inside of the infection radius will be infected on each tick */
+    private double infectionChance = 0.5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Person : MonoBehaviour
+{
+    private bool isWearingMask = false;
+    private bool isVaccinated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setStartingPosition();
+    }
+
+    private void setStartingPosition()
+    {
+        this.transform.position = new Vector3(Random.Range(0, Screen.width), this.transform.position.y,
+            Random.Range(0, Screen.height));
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

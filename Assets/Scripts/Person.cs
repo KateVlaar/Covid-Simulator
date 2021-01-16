7404e4f0f51abe3458c70df9a7f3d2ae5bc54448
naviza <43558584+naviza@users.noreply.github.com>
2021-01-16 14:32:36 -0700

    Add Random Person movement

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -9,6 +9,9 @@ public class Person : MonoBehaviour
     private bool isWearingMask = false;
     private bool isVaccinated = false;
 
+    public Rigidbody2D rb;
+    public float radius = .0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,9 +31,16 @@ public class Person : MonoBehaviour
         this.transform.position = new Vector3(Random.Range(boundX, boundX), Random.Range(boundY, boundY),
             this.transform.position.z);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(
+                Random.Range(-1 * radius, radius),
+                Random.Range(-1 * radius, radius)
+                );
+        }
     }
 }

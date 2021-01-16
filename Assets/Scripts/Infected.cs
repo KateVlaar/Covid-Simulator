313f327f0707b915b541a44c7bfeea661d9965ea
IsaakC <gcoleman@ualberta.ca>
2021-01-16 15:26:13 -0700

    cancer

--- Assets/Scripts/Infected.cs
+++ Assets/Scripts/Infected.cs
@@ -7,11 +7,16 @@ public class Infected : Person
     private double infectionRadius = 5.0;
     /* Chance out of 0.5 that a susceptible person inside of the infection radius will be infected on each tick */
     private double infectionChance = 0.5;
+
+    SpriteRenderer m_SpriteRenderer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Fetch the SpriteRenderer from the GameObject
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        //Set the GameObject's Color quickly to a set Color (blue)
+        m_SpriteRenderer.color = Color.red;
     }
 
     // Update is called once per frame

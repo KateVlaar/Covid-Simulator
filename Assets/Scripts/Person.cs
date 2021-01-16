7635fe3aa6c6878a19d33f52a9bbe7f205a6aa77
Hudson <huddy987@gmail.com>
2021-01-16 15:03:21 -0700

    Smooth random movement

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,16 +9,20 @@ public class Person : MonoBehaviour
 {
     private bool isWearingMask = false;
     private bool isVaccinated = false;
+    private double velocityChangeTime = 2.0f;
 
     public Rigidbody2D rb;
-    public float radius = .0f;
 
     // Start is called before the first frame update
     void Start()
     {
         setStartingPosition();
+        chooseNewRandomDelay();
     }
 
+    /**
+     * Sets the starting position within the camera.
+     */
     private void setStartingPosition()
     {
         Camera cam = Camera.main;
@@ -31,16 +36,26 @@ public class Person : MonoBehaviour
         this.transform.position = new Vector3(Random.Range(-boundX, boundX), Random.Range(-boundY, boundY),
             this.transform.position.z);
     }
+
+    /**
+     * Chooses a new delay for the next time to update the velocity
+     */
+    private void chooseNewRandomDelay()
+    {
+        this.velocityChangeTime = Random.Range(0.5f, 5.0f);
+    }
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        this.velocityChangeTime -= Time.deltaTime;
+        if (this.velocityChangeTime <= 0.0f)
         {
             rb.velocity = new Vector2(
-                Random.Range(-1 * radius, radius),
-                Random.Range(-1 * radius, radius)
-                );
+                Random.Range(-1.0f, 1.0f),
+                Random.Range(-1.0f, 1.0f)
+            );
+            this.chooseNewRandomDelay();
         }
     }
 }

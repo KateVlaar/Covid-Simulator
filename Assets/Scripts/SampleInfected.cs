32eaf2c8a707a41724b638b0562570180f77ae45
Hudson <huddy987@gmail.com>
2021-01-17 09:14:23 -0700

    Add missing files for showing sample radius

---
+++ Assets/Scripts/SampleInfected.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Sample infected class to visualize infection radius in the main menu
+ */
+public class SampleInfected : MonoBehaviour
+{
+    public void setInfectionRadius(float radius)
+    {
+        this.GetComponent<CircleCollider2D>().radius = radius;
+        this.GetComponent<Light>().range = radius*2;
+    }
+}
